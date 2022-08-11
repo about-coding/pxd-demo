314b55ab73fd7cd899e4b3ec75c95a4844e13d87
Manuel Klein <manuel.klein@prodyna.com>
2022-08-11 14:31:03 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Amplify } from 'aws-amplify';
+// import { Amplify } from 'aws-amplify';
 
 import Router from './routes';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
-import awsconfig from './aws-exports';
+// import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 export default function App() {
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import { API } from 'aws-amplify';
-import { listProducts } from '../graphql/queries';
+// import { listProducts } from '../graphql/queries';
 
 export async function fetchAllProducts() {
-  const response = await API.graphql({ query: listProducts });
+  // const response = await API.graphql({ query: listProducts });
+  const response = {};
 
   return response.data;
 }
